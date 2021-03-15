79615b0a7ce7b5e47df2c5033d456ce1d27adf44
tunguyen <tunguyen060811@gmail.com>
2021-03-15 11:38:45 +0700

    fix models.py after REVIEW

--- stadium/book_stadium/models.py
+++ stadium/book_stadium/models.py
@@ -1,50 +1,60 @@
 from django.db import models
-CHOICES = (
-    ('admin', 'admin'),
-    ('customer', 'customer')
-)
+from django.contrib.auth.models import AbstractUser
+
+
+class Roles(models.TextChoices):
+    OWNER = 'owner', 'Chủ sân'
+    PLAYER = 'player', 'Người đặt'
+
 # Create your models here.
-class User(models.Model):
-    username = models.CharField(max_length=100)
-    name = models.CharField(max_length=100, null=True, blank=False)
-    phone_number = models.CharField(max_length=12, null=True, blank=False)
-    role = models.CharField(max_length=20, null=True, choices=CHOICES)
-    email = models.EmailField(max_length=100, null=True)
+
+
+class User(AbstractUser):
+    username = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=100, blank=True)
+    phone_number = models.CharField(max_length=12, blank=True)
+    role = models.CharField(max_length=20, choices=Roles.choices)
+
+    REQUIRED_FIELDS = ['phone_number']
 
     def __str__(self):
-        return self.name
-    
+        return self.username
+
 
 class Stadium(models.Model):
-    name = models.CharField(max_length=100, blank=False)
-    address = models.CharField(max_length=100, null=True, blank=False)
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100, blank=True)
     owner = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
-    filed_count = models.IntegerField(blank=False)
+    field_count = models.PositiveSmallIntegerField()
+    time_frames = models.ManyToManyField('TimeFrame', through='StadiumTimeFrame')
 
     def __str__(self):
         return f'{self.name}-{self.address}'
 
+
 class TimeFrame(models.Model):
-    start_time = models.TimeField(blank=False)
-    end_time = models.TimeField(blank=False)
+    start_time = models.TimeField()
+    end_time = models.TimeField()
 
     def __str__(self):
         return f'{self.start_time} - {self.end_time}'
 
+
 class StadiumTimeFrame(models.Model):
-    stadium = models.ForeignKey(Stadium, null=True, on_delete=models.SET_NULL)
-    time_frame = models.ForeignKey(TimeFrame, null=True, on_delete=models.SET_NULL)
-    price = models.IntegerField(blank=False)
+    stadium = models.ForeignKey(Stadium, on_delete=models.CASCADE)
+    time_frame = models.ForeignKey(TimeFrame, on_delete=models.CASCADE)
+    price = models.IntegerField()
 
     def __str__(self):
         return f'{self.stadium} - {self.time_frame}'
 
+
 class Order(models.Model):
     user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
-    stadium_time_frame = models.ForeignKey(StadiumTimeFrame, null=True,on_delete=models.SET_NULL)
-    field_number = models.IntegerField()
+    stadium_time_frame = models.ForeignKey(StadiumTimeFrame, null=True, on_delete=models.SET_NULL)
+    field_number = models.PositiveSmallIntegerField()
     order_datetime = models.DateTimeField(auto_now_add=True)
-    is_accepted = models.BooleanField(default=False)
+    is_accepted = models.BooleanField()
     customer_phone_number = models.CharField(max_length=12)
     customer_name = models.CharField(max_length=100)
 
