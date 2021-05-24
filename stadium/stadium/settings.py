a37238102a042bf4d42a18ef6476d519c69e451e
trungtrantoday <trungtrantoday@gmail.com>
2021-05-24 15:48:18 +0700

    setup django-environ

--- stadium/stadium/settings.py
+++ stadium/stadium/settings.py
@@ -12,6 +12,17 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import environ
+
+env = environ.Env(
+    DEBUG=(bool, True),
+    SECRET_KEY=(str, 'nothingtoseehere'),
+    DATABASE_URL=(str, 'mysql://root:admin123@localhost:3306/stadium_db_2'),
+    ALLOWED_HOSTS=(list, ['testserver', 'localhost', '127.0.0.1']),
+    SELENIUM_CHROME_DRIVER_PATH=(str, ''),
+)
+
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +32,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'bk1uo^ngjw)p((3+j2y9mjfukyy+15*5en!)&ciiw3yhx+-pg&'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env('ALLOWED_HOSTS')
 
 # Application definition
 
@@ -42,6 +53,7 @@ INSTALLED_APPS = [
     'book_stadium',
     'social_django',
     'notifications',
+    'behave_django',
 ]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
@@ -80,14 +92,7 @@ AUTH_USER_MODEL = 'book_stadium.User'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'stadium_db_2',
-        'HOST': 'localhost',
-        'PORT': 3306,
-        'USER': 'root',
-        'PASSWORD': 'admin123',
-    }
+    'default': env.db(),
 }
 
 
@@ -158,3 +163,5 @@ EMAIL_HOST_USER = 'tunv011199@gmail.com'
 EMAIL_HOST_PASSWORD = 'tuvip123'
 
 USE_THOUSAND_SEPARATOR = True
+
+SELENIUM_CHROME_DRIVER_PATH = env('SELENIUM_CHROME_DRIVER_PATH')
