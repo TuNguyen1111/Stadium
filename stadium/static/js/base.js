b9c5cf73cac8082e2743dff971a16be066fdce7a
tunguyen <tunguyen060811@gmail.com>
2021-05-29 01:05:08 +0700

    check if not have sidebar then add property margin-left for element

--- stadium/static/js/base.js
+++ stadium/static/js/base.js
@@ -1,30 +1,35 @@
 $(document).ready(function(){
-    $('.manage-btn').click(function() {
+    $('.manage-btn').on('click', function() {
         $('ul li .first').toggleClass('rotate');
         $('.stadium-manage').slideToggle();
     });
 
-    $('.state-btn').click(function() {
+    $('.state-btn').on('click', function() {
         $('ul li .second').toggleClass('rotate');
         $('.stadium-state').slideToggle();
     });
 
-    $('.toggle-login-modal').click(function() {
+    $('.toggle-login-modal').on('click', function() {
         $('#login-modal').modal('show');
     });
 
-    $('.toggle-register-modal').click(function() {
+    $('.toggle-register-modal').on('click', function() {
         $('#login-modal').modal('hide');
         $('#register-modal').modal('show');
     });
 
     if ( $('#error_1_id_email_or_phone').length ) {
         $('#register-modal').modal('show');
-        console.log($('#error_1_id_email_or_phone'))
     }
 
     if ( $(`#message-error`).length ) {
         $('#login-modal').modal('show');
+    }
+
+    console.log($('#sidebar').length)
+    if (!$('#sidebar').length) {
+        console.log('dsdfsdf')
+        $('.col').css('marginLeft', '0')
     }
 
     $('#id_role').addClass('form-select')
