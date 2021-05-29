204b830ad613ee49a6ae244259f13116896eef97
tunguyen <tunguyen060811@gmail.com>
2021-05-30 00:08:31 +0700

    create function to clear message

--- stadium/static/js/base.js
+++ stadium/static/js/base.js
@@ -4,38 +4,48 @@ $(document).ready(function(){
     showModalIfHaveError() 
     addClassForSelectType()
     setPropertyIfNoSidebar()
+    clearMessage()
 })
+
+function clearMessage() {
+    let message = $('#message')
+    if (message.length) {
+        setTimeout(function() {
+            message.remove()
+        }, 2000)
+    }
+}
 
 function setEventForButtonInSidebar() {
     $('.manage-btn').on('click', function() {
-        $('ul li .first').toggleClass('rotate');
-        $('.stadium-manage').slideToggle();
-    });
+        $('ul li .first').toggleClass('rotate')
+        $('.stadium-manage').slideToggle()
+    })
 
     $('.state-btn').on('click', function() {
-        $('ul li .second').toggleClass('rotate');
-        $('.stadium-state').slideToggle();
-    });
+        $('ul li .second').toggleClass('rotate')
+        $('.stadium-state').slideToggle()
+    })
 }
 
 function setEventForLoginAndRegisterBtn() {
     $('.toggle-login-modal').on('click', function() {
-        $('#login-modal').modal('show');
-    });
+        $('#login-modal').modal('show')
+    })
 
     $('.toggle-register-modal').on('click', function() {
-        $('#login-modal').modal('hide');
-        $('#register-modal').modal('show');
-    });
+        $('#login-modal').modal('hide')
+        $('#register-modal').modal('show')
+    })
 }
 
 function showModalIfHaveError() {
     if ( $('#error_1_id_email_or_phone').length ) {
-        $('#register-modal').modal('show');
+        $('#register-modal').modal('show')
     }
 
     if ( $(`#message-error`).length ) {
-        $('#login-modal').modal('show');
+        $('#login-modal').modal('show')
     }
 }
 
