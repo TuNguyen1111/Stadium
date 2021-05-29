5a0dae3a78aa29f873ec7e018b9fc3dbd6c0c402
tunguyen <tunguyen060811@gmail.com>
2021-05-30 00:13:43 +0700

    delete ;

--- stadium/static/js/search_stadium.js
+++ stadium/static/js/search_stadium.js
@@ -1,52 +1,52 @@
 $(document).ready(function() {
-    showNotificationModal();
-    toggleSearchAndOrderForm();
+    showNotificationModal()
+    toggleSearchAndOrderForm()
     setEventForOrderBtn()
 })
 
 function showNotificationModal() {
     if ( $('#success').length ) {
-        $('#notification-success').modal('show');
+        $('#notification-success').modal('show')
     }
     else if ( $('#warning').length ) {
-        $('#notification-failed').modal('show');
-    };
+        $('#notification-failed').modal('show')
+    }
 }
 
 function toggleSearchAndOrderForm() {
     $('.search-btn-dropdown').click(function() {
-        $('.search-show').slideToggle();
-    });
+        $('.search-show').slideToggle()
+    })
 
     $('.order-btn-dropdown').click(function() {
-        $('.order-show').slideToggle();
-    });
+        $('.order-show').slideToggle()
+    })
 }
 
 function setEventForOrderBtn() {
     $('.order-btn').click(function(e) {
         let currentItem = $(e.target)
-        let stadiumName = currentItem.attr('stadium-name');
-        let userName = currentItem.attr('user-name');
-        let userPhoneNumber = currentItem.attr('user-phone-number');
-        let timeframe = currentItem.attr('time-frame');
+        let stadiumName = currentItem.attr('stadium-name')
+        let userName = currentItem.attr('user-name')
+        let userPhoneNumber = currentItem.attr('user-phone-number')
+        let timeframe = currentItem.attr('time-frame')
 
-        let stadiumInput = $('#stadium-name');
-        let userNameInput = $('#id_customer_name');
-        let userPhoneNumberInput = $('#id_customer_phone_number');
-        let timeframeInput = $('.form-order #time_frame');
+        let stadiumInput = $('#stadium-name')
+        let userNameInput = $('#id_customer_name')
+        let userPhoneNumberInput = $('#id_customer_phone_number')
+        let timeframeInput = $('.form-order #time_frame')
         
-        stadiumInput.val(stadiumName);
-        userNameInput.val(userName);
-        userPhoneNumberInput.val(userPhoneNumber);
+        stadiumInput.val(stadiumName)
+        userNameInput.val(userName)
+        userPhoneNumberInput.val(userPhoneNumber)
 
         if ( timeframe ) {
-            timeframeInput.val(timeframe);
+            timeframeInput.val(timeframe)
         }
 
         if ( userName ) {
-            userNameInput.val(userName);
-            userPhoneNumberInput.val(userPhoneNumber);
+            userNameInput.val(userName)
+            userPhoneNumberInput.val(userPhoneNumber)
         }
 
         $('#book-stadium').modal('show')
